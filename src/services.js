e35c0d66a77f41378695a46d1d9448ce71d3a5cd
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-02-16 01:36:10 -0500

    Baseline 1.13
    Update to add modal for the Services

--- src/services.js
+++ src/services.js
@@ -7,6 +7,8 @@ import Contact from './contact'
 
 function Services() {
     const [desktopState,setDesktopState] = useState('Services1');  
+    const [showModal, setShowModal] = useState(false);
+    const [selectedService, setSelectedService] = useState(null);
 
     const handleAboutClick = () => {
         setDesktopState('AboutUs');
@@ -20,6 +22,70 @@ function Services() {
       const handleContactClick = () => {
         setDesktopState('Contact');
       };
+
+      const Modal = ({ onClose, service }) => {
+        return (
+        // <div class="portfolio-modal modal fade" id="serviceitem1" tabindex="-1" role="dialog" aria-hidden="true">
+        //     <div class="modal-dialog">
+        //         <div class="modal-content">
+        //             <div class="close-modal p-2" data-bs-dismiss="modal"><img src="images/close-icon.svg" alt="Close modal" /></div>
+        //             <div class="container">
+        //                 <div class="row justify-content-center">
+        //                     <div class="col-lg-12">
+        //                         <div class="modal-body">
+        //                             <!-- Project details-->
+        //                             <h2 class="text-uppercase">Lifting Code Luxury Facial</h2>
+        //                             <img class="img-fluid d-block mx-auto" src="images/brochure/TREATMENT_2.jpg" alt="Luxury Facial" />
+        //                             <p class="card-text">For a lifted and re plumped look. This treatment works on 
+        //                               minimizing all types of lines and wrinkles, this luxury facial 
+        //                               treatment is specially designed to improve skins hydration 
+        //                               levels, skin tone and firmness, formulas with highly 
+        //                               concentrated natural collagen and hyaluronic acid outline 
+        //                               the profile of face and neck.</p>
+        //                             <span class="px-1 price">Price: $189</span>
+        //                             <span class="px-1 price" style="font-weight:normal">70 mins</span>
+        //                             <a style="float: none;" target="_blank" href="https://booking.setmore.com/scheduleappointment/35bbcd9a-2c93-4660-90df-9873ef5c488d/services/bbd4c531-ef0c-44a6-8066-4f8e62218665?source=easyshare"><img border="none" src="https://storage.googleapis.com/full-assets/setmore/images/1.0/Settings/book-now-blue.svg" alt="Book an appointment with Paul M using Setmore"/></a>
+        //                             <div style="padding-top: 2rem"></div>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+          <div className="portfolio-modal fade modal" style={{border:"solid gray 4px", scrollBehavior:'', background:'gray',width:'90vw',top:'10%',left:'5vw',height:'80vh',position:'fixed', display:'flex', justifyContent: '', alignItems: '',zIndex:'6'}}>
+            <div className="modal-content">
+                <div style={{margin:'15px'}}>
+                    <span className="close" style={{cursor:'pointer', fontSize:'40px', display:'flex',position:'absolute', right:20 }}onClick={onClose}>&times;</span>
+                    <h2>{service.title}</h2>
+                    <p>{service.description}</p>
+                </div>
+                    <img src={service.image}></img>
+            </div>
+          </div>
+        );
+      };
+
+
+    const services = [
+        { title: "Assessments", description: "Assessments include state analysis and an implementation roadmap.",image:'./boardroom-queenst2.jpg' },
+        { title: "Organization Audits", description: "Detailed audits of your organization.",image:"./boardroom-queenst.jpg" },
+        { title: "Program Evaluation", description: "Comprehensive analysis that is used to maintain an effectiveness program",image:"./groupmeeting.jpeg" },
+        { title: "Strategic Plan", description: "Stragetic Plan for your organization",image:"./boardroom-queenst.jpg" },
+
+        // Add more services as needed
+    ];
+
+    const handleReadMoreClick = (service) => {
+        setSelectedService(service);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedService(null);
+    };
   
   return (
     
@@ -38,22 +104,28 @@ function Services() {
                             </div>
                         </div>
                     </div>
-                    <div className="BodyX" style={{flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 48, display: 'inline-flex'}}>
-                        <div className="Servicecards1">
-                            <div className="Frame2230">
-                                <div className="Servicecard">
-                                    <div className="CardImage">
-                                        <img className="cardpic" src="./boardroom-brightwider.jpg" alt='boardroom'/>
-                                    </div>
-                                    <div className="CardTextx">Assessments include state analysis and an implementation roadmap.</div>
-                                    <div className="Group5">
-                                        <div className="TitleSer">Assessments</div>
-                                    </div>
-                                    <div className="Button">
-                                        <div className="ReadMore">READ MORE</div>
+                    <div className="BodyX" style={{maxWidth:'100vw',flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 48, display: 'inline-flex'}}>
+                        <div className="Servicecards1" >
+                            <div className="Frame2230"style={{position:'relative',right:"5%",}}>
+                                <div className="Servicecards1">
+                                    <div className="Frame2230">
+                                        {services.map((service, index) => (
+                                        <div className="Servicecard" key={index}>
+                                            <div className="CardImage">
+                                            <img className="cardpic" src={`./${service.image}`} alt={service.title} />
+                                            </div>
+                                            <div className="CardTextx">{service.description}</div>
+                                            <div className="Group5">
+                                            <div className="TitleSer">{service.title}</div>
+                                            </div>
+                                            <div className="Button">
+                                            <div className="ReadMore" onClick={() => handleReadMoreClick(service)}>READ MORE</div>
+                                            </div>
+                                        </div>
+                                        ))}
                                     </div>
                                 </div>
-                                <div className="Servicecard">
+                                {/* <div className="Servicecard">
                                     <div className="CardImage">
                                         <img className="cardpic" src="./boardroom-queenst2.jpg" alt='boardroom'/>
                                     </div>
@@ -88,7 +160,8 @@ function Services() {
                                     <div className="Button">
                                         <div className="ReadMore">READ MORE</div>
                                     </div>
-                                </div>
+                                </div> */}
+                                {showModal && <Modal onClose={handleCloseModal} service={selectedService} />}
                             </div>
                         </div>
                         <div className="IntroSection" style={{alignSelf: 'stretch', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
